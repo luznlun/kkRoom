d40ba97e2cda2ff577b5df79a6f56a0db1ac51b0
chris <chris@readytouch.com>
2016-09-20 11:01:01 -0700

    setup bootstrap

--- src/components/Html.js
+++ src/components/Html.js
@@ -10,12 +10,16 @@ function Html({ title, description, style, script, children }) {
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         {style && <style id="css" dangerouslySetInnerHTML={{ __html: style }} />}
       </head>
       <body>
         <div id="app" dangerouslySetInnerHTML={{ __html: children }} />
         {script && <script src={script} />}
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
       </body>
     </html>
   );
